daa855062adb6575f75e3137128256bfeb04e15c
DanieleFic <reb_ficus22@hotmail.it>
2021-12-16 15:56:55 +0100

    snack 3

--- jsnack2/myscript.js
+++ jsnack2/myscript.js
@@ -7,8 +7,15 @@ Ogni volta che ne crei uno, stampalo a schermo. */
 let N = prompt("inserici numero");
 
 for(let i = 0; i < N; i++){
-    let myArray = [Math.floor((Math.random() * 100)+1)] * 10;
+    let myArray = [];
+
+    for (index= 0; index < 10; index++){
+        myArray.push(Math.floor((Math.random() * 100)+1)) ;
+        
+    }
     console.log(myArray)
-}
+};
+
+
 
 
